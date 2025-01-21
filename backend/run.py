c766b368e5123a1f66cef433cd9d79d940720974
Leonardo Quintero <leonardo9526@gmail.com>
2025-01-21 12:11:31 -0500

    init commit

--- backend/run.py
+++ backend/run.py
@@ -4,5 +4,4 @@ import uvicorn
 app = create_app()
 
 if __name__ == "__main__":
-    # Ejecuta el servidor ASGI usando Uvicorn directamente desde Python
     uvicorn.run(app, host="0.0.0.0", port=5000, log_level="debug")
